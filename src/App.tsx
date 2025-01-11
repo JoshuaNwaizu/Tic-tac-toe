97d1511c90973e1e11d6e25477ca20ca9991aa6e
JoshuaNwaizu <joshuachris000@gmail.com>
2025-01-11 18:12:31 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-svh w-svw ">
+      <h1 className="font-bold text-[4rem]">Tic Tac Toe</h1>
+    </div>
+  );
+}
+
+export default App;
